8af498ea0fe20320998214e8203fc34e663ee202
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-22 17:23:29 +0500

    CanCan authorization added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to users_path, alert: exception.message
+  end
+
   before_action :authenticate_user!
 
   layout 'admin_lte_2'
