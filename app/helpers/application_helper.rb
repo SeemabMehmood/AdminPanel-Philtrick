658cf9c2672c58f0e169e31363eee39ec08a5681
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-24 02:48:22 +0500

    code refactor admin page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
     return 'active' if request.path == '/' && current_page == 'dashboard'
     request.path.include?(current_page) ? 'active' : ''
   end
+
+  def breadcrumb_action_name
+    return "Customer" if titleize(params[:controller].parameterize) == "Users"
+    titleize(params[:controller].parameterize)
+  end
 end
