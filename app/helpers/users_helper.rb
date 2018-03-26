b6e70bd06dce28d18d4ae974ab7042b441d0311e
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-27 03:18:19 +0500

    User form submit button text updated

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def get_selected_country(user)
     user.country if user.new_record?
   end
+
+  def form_submit_text(form_object)
+    form_object.persisted? ? "Update Customer" : "Add Customer"
+  end
 end
