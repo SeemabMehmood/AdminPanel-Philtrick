0a51fa091d2adb949997b1cb6173554e957efb30
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-28 10:53:37 +0500

    Updated dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def dashboard
-    @workers = Worker.all
-    @offline_workers = Worker.offline
+    if current_user.admin
+      @workers = Worker.all
+      @offline_workers = Worker.offline
+    else
+      @workers = Worker.get_customer_workers(current_user.id)
+    end
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def to_btc(amount)
+    amount * 0.00013
+  end
+
+  def show_income(income)
+    return income if income.present?
+    0.0
+  end
 end
