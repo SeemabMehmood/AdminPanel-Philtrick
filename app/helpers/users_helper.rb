879565e19509b97e112348901942c5b20c0166cc
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-28 18:57:19 +0500

    User/Worker form code refactor

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,18 +7,13 @@ module UsersHelper
     form_object.persisted? ? "Update #{object_type}" : "Add #{object_type}"
   end
 
-  def back_user_form_url(action_name)
-    return users_path if action_name == 'index'
+  def back_user_form_url(action_name, user)
+    return users_path if action_name == 'index' || user.new_record?
     current_user if action_name == 'show' || action_name.blank?
   end
 
   def back_user_show_url
     return users_path if current_user.admin
     authenticated_root_path
-  end
-
-  def back_worker_form_url(action_name, worker)
-    return workers_path if action_name == 'index'
-    worker_path(worker) if action_name == 'show' || action_name.blank?
   end
 end
