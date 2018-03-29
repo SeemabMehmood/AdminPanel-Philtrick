348aa82d816bbd31d4b28b1732dfc12c60d9909d
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-29 22:23:03 +0500

    Updated recatcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6Lc7VE4UAAAAAAh61AqMk93eFGmydI93GJKPV-wN'
-  config.secret_key = '6Lc7VE4UAAAAAL5hoNVQySG4y9gEiqm4OsFZUHfX'
+  config.site_key  = '6LdAxU8UAAAAALRJ4Wo3fYiLYW1d47aZpdrk6OJ7'
+  config.secret_key = '6LdAxU8UAAAAAOxUBZzlCl08f5x60j8WFU29wlXW'
 end
