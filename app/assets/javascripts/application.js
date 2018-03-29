06c57ab00d744dda3644293715aeb6cf7422db49
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-30 02:37:32 +0500

    Bootstrap console error removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap
 //= require jquery
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require Chart.min
