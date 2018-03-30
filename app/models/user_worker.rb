bf762547e824ef5fdd14d4ba9fcc5bc4c43f093c
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-03-30 23:11:27 +0500

    Lists of workers and customers updated

--- app/models/user_worker.rb
+++ app/models/user_worker.rb
@@ -1,4 +1,7 @@
 class UserWorker < ApplicationRecord
   belongs_to :user, inverse_of: :user_workers
   belongs_to :worker, inverse_of: :user_workers
+
+  scope :for_user, -> (user_id) { where(user_id: user_id).last }
+  scope :for_worker, -> (worker_id) { where(worker_id: worker_id).last }
 end
