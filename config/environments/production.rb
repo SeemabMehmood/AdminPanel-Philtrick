b9b2035b7e732042b93e71c9321e8262700d37ca
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-04-07 15:27:01 +0500

    Added smtp settings for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'www.philtrickminers.de' }
+  config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      user_name: "app92318103@heroku.com",
+      password: "z3sakqeb6786",
+      domain: 'www.philtrickminers.de',
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
