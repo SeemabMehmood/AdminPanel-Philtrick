dcb40a6196b48c49b09baf97723f90244c9128fa
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-04-28 11:57:24 +0500

    Users rehgiration create method override

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :require_no_authentication
 
+  def create
+    @user = User.new(sign_up_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def after_sign_up_path_for(resource)
     user_path(resource)
   end
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :username, :company_name, :zip, :country, :street_name, :profit_share, :net_income, :worker_id)
+    params.require(:user).permit(:name, :email, :username, :company_name, :zip, :country, :street_name, :profit_share)
   end
 end
