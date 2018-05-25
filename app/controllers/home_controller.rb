d91da09204378ab7593349487acfa6060dc2fd4c
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-05-25 20:56:47 +0500

    Show worker count for each currency on dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,15 +9,15 @@ class HomeController < ApplicationController
       @daily_deposits = @latest_deposits.group_by { |d| [d.date, d.worker] }
     else
       @customer_workers = Worker.get_customer_workers(current_user)
-      @litecoin_workers = @customer_workers.get_currency_workers('LTC')
+      @litecoin_workers = @customer_workers.get_currency_workers('LTC').sum(:worker_count)
       @litecoin_income_today = current_user.income_today_for_workers(@litecoin_workers)
       @litecoin_net_income = current_user.ltc_net_income
 
-      @bitcoin_workers = @customer_workers.get_currency_workers('BTC')
+      @bitcoin_workers = @customer_workers.get_currency_workers('BTC').sum(:worker_count)
       @bitcoin_income_today = current_user.income_today_for_workers(@bitcoin_workers)
       @bitcoin_net_income = current_user.btc_net_income
 
-      @bitcash_workers = @customer_workers.get_currency_workers('BCH')
+      @bitcash_workers = @customer_workers.get_currency_workers('BCH').sum(:worker_count)
       @bitcash_income_today = current_user.income_today_for_workers(@bitcash_workers)
       @bitcash_net_income = current_user.bch_net_income
     end
