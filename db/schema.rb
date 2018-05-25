66882efe65dec970e6bae899c20fb3c4ec9acbe2
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-05-25 10:13:02 +0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180521040905) do
+ActiveRecord::Schema.define(version: 20180525044306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,9 @@ ActiveRecord::Schema.define(version: 20180521040905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "mining_address"
+    t.decimal "btc_net_income", precision: 25, scale: 12, default: "0.0"
+    t.decimal "ltc_net_income", precision: 25, scale: 12, default: "0.0"
+    t.decimal "bch_net_income", precision: 25, scale: 12, default: "0.0"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
