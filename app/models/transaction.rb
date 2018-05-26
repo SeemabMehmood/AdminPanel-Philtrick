8bb11856775285a50d05b4d4043403bf5017f762
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-05-26 09:48:33 +0500

    Status change dropdown added

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,5 +4,11 @@ class Transaction < ApplicationRecord
 
   validates :amount, numericality: {less_than_or_equal_to: 99999999999, message: "must be less than 10 Billion"}
 
+  default_scope { order(created_at: :desc) }
+
   STATUS = ["Approved", "Pending", "Rejected"]
+
+  def is_pending?
+    ['pending', 'Pending'].include? self.status
+  end
 end
